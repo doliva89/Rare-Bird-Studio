ccb80ef69a71d8f9c6a2574f5ef5fc78f63bc222
David Oliva <davidoliva89@hotmail.com>
2017-10-26 13:04:40 +1100

    +First iteration of music manager scripts +starting of crossfade script for music +rara music and demo for testing

---
+++ Rara-Avis/Assets/Code/Scripts/Audio/CrossFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class CrossFade : MonoBehaviour {
+
+    public static CrossFade Instance;
+
+    void Awake(){
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void Crossfade(Audio)
+}
