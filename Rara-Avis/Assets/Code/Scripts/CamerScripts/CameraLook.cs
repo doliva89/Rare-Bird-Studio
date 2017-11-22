4ee5aa9258f33d0042d8eedcd586f6c4e69e5d0d
AIE\s171235 <snowy844@gmail.com>
2017-11-22 12:11:22 +1100

    skybox camera set up

--- Rara-Avis/Assets/Code/Scripts/CamerScripts/CameraLook.cs
+++ Rara-Avis/Assets/Code/Scripts/CamerScripts/CameraLook.cs
@@ -4,36 +4,14 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour {
 
-  
-    
-    public float turnSpeed = 10f;
-    public Transform player;
-    public float radius;
-   
-    private Vector3 offsetY;
-    
-    // Use this for initialization
-    void Start () {
+    public Transform main;
 
-        offsetY = new Vector3(0, 2, -5);
+    void Start() {
+        main = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
     }
 
-    void FixedUpdate() {
-       
+    void Update() {
+
+        transform.rotation = main.transform.rotation;
     }
-    void LateUpdate() {
-
-        offsetY = Quaternion.AngleAxis(/*Input.GetAxis("Mouse Y") ||*/ Input.GetAxisRaw("RightThumbV") * turnSpeed, -Vector3.right) * offsetY;
-        //print(offsetY);
-
-        //clamp the y and z values of camera
-        offsetY.y = Mathf.Clamp(offsetY.y, 0.2f, 5.3f);
-        offsetY.z = Mathf.Clamp(offsetY.z, -5.3f, -.3f);
-
-        transform.position = player.TransformPoint( offsetY);
-        transform.LookAt(player.position);
-       
-
-    }
-    
 }
